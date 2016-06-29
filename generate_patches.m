f91f57eb7bec6dd719fa9cb0cecb155501c78123
rogertrullo <rogertrullo@hotmail.com>
2016-06-29 18:26:41 -0400

    new version to generate one h5 for each patient

--- generate_patches.m
+++ generate_patches.m
@@ -1,3 +1,4 @@
+%%
 clear all
 clc
 
@@ -7,14 +8,19 @@ direc= dir(pathpatients);
 patientlist={direc.name}';
 patientlist=patientlist(3:end);%ubuntu in mac is 4
 patch_sz=[32,32,16];
-nsamples=800;
+nsamples=700;
 nclasses=5;
 idpatch=1;
-X=zeros(length(patientlist)*nsamples*nclasses,1,patch_sz(1),patch_sz(2),patch_sz(3));
-y=zeros(length(patientlist)*nsamples*nclasses,patch_sz(1),patch_sz(2),patch_sz(3));
+% X=zeros(length(patientlist)*nsamples*nclasses,1,patch_sz(1),patch_sz(2),patch_sz(3));
+% y=zeros(length(patientlist)*nsamples*nclasses,patch_sz(1),patch_sz(2),patch_sz(3));
+
+X=zeros(patch_sz(2),patch_sz(1),patch_sz(3),1,nsamples*nclasses);
+y=zeros(patch_sz(2),patch_sz(1),patch_sz(3),nsamples*nclasses);
 
 
-for idpatient=1:length(patientlist)%for every patient
+
+%for idpatient=1:length(patientlist)%for every patient
+for idpatient=1:5%just to test
     name=patientlist{idpatient};
     
     body=load(strcat(pathpatients,name,'/contour.mat'));
@@ -73,49 +79,78 @@ for idpatient=1:length(patientlist)%for every patient
     
     for i=1:length(list_rndbg)
         [r,c,z]=ind2sub(size(ctvol),list_rndbg(i));       
-        X(idpatch,1,:,:,:)=ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
-        y(idpatch,:,:,:)=segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+        %X(idpatch,1,:,:,:)=ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+        %y(idpatch,:,:,:)=segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+        
+        X(:,:,:,1,idpatch)=permute(ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1),[2 1 3]);
+        y(:,:,:,idpatch)=permute(segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1),[2 1 3]);
+        
         idpatch=idpatch+1;
     end
     
     for i=1:length(list_rndeso)
         [r,c,z]=ind2sub(size(ctvol),list_rndeso(i));       
-        X(idpatch,1,:,:,:)=ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
-        y(idpatch,:,:,:)=segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+%         X(idpatch,1,:,:,:)=ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+%         y(idpatch,:,:,:)=segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+        X(:,:,:,1,idpatch)=permute(ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1),[2 1 3]);
+        y(:,:,:,idpatch)=permute(segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1),[2 1 3]);
         idpatch=idpatch+1;
     end
     
     for i=1:length(list_rndheart)
         [r,c,z]=ind2sub(size(ctvol),list_rndheart(i));       
-        X(idpatch,1,:,:,:)=ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
-        y(idpatch,:,:,:)=segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+%         X(idpatch,1,:,:,:)=ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+%         y(idpatch,:,:,:)=segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+        X(:,:,:,1,idpatch)=permute(ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1),[2 1 3]);
+        y(:,:,:,idpatch)=permute(segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1),[2 1 3]);
         idpatch=idpatch+1;
     end
     
     for i=1:length(list_rndtrach)
         [r,c,z]=ind2sub(size(ctvol),list_rndtrach(i));       
-        X(idpatch,1,:,:,:)=ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
-        y(idpatch,:,:,:)=segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+%         X(idpatch,1,:,:,:)=ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+%         y(idpatch,:,:,:)=segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+        X(:,:,:,1,idpatch)=permute(ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1),[2 1 3]);
+        y(:,:,:,idpatch)=permute(segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1),[2 1 3]);
         idpatch=idpatch+1;
     end
     
     for i=1:length(list_rndaorta)
         [r,c,z]=ind2sub(size(ctvol),list_rndaorta(i));       
-        X(idpatch,1,:,:,:)=ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
-        y(idpatch,:,:,:)=segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+%         X(idpatch,1,:,:,:)=ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+%         y(idpatch,:,:,:)=segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1);
+        X(:,:,:,1,idpatch)=permute(ctvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1),[2 1 3]);
+        y(:,:,:,idpatch)=permute(segvol(r:r+patch_sz(1)-1,c:c+patch_sz(2)-1,z:z+patch_sz(3)-1),[2 1 3]);
         idpatch=idpatch+1;
     end
     
-    
+    idpatch=1;
     disp([name,' done'])
     
-end
-disp('all patches in memory')
-%%
-h5create('/media/roger/48BCFC2BBCFC1562/train.h5','/data',size(X),'Datatype','double');
-h5create('/media/roger/48BCFC2BBCFC1562/train.h5','/label',size(y),'Datatype','double');
+    rndidx=randperm(size(X,5));
+    X=X(:,:,:,:,rndidx);
+    y=y(:,:,:,rndidx);
+    disp('all patches in memory')
+    
+    h5create(sprintf('/media/roger/48BCFC2BBCFC1562/train%d.h5',idpatient),'/data',size(X),'Datatype','double');
+    h5create(sprintf('/media/roger/48BCFC2BBCFC1562/train%d.h5',idpatient),'/label',size(y),'Datatype','double');
 
-h5write('/media/roger/48BCFC2BBCFC1562/train.h5','/data',X);
-h5write('/media/roger/48BCFC2BBCFC1562/train.h5','/label',y);
+    h5write(sprintf('/media/roger/48BCFC2BBCFC1562/train%d.h5',idpatient),'/data',X);
+    h5write(sprintf('/media/roger/48BCFC2BBCFC1562/train%d.h5',idpatient),'/label',y);
+    
+end
+
+
+%%
+% patients_save=5;
+% 
+% for i=1:patients_save
+%     h5create(sprintf('/media/roger/48BCFC2BBCFC1562/train%d.h5',i),'/data',size(X(:,:,:,:,(i-1)*nsamples*nclasses+1:i*nsamples*nclasses)),'Datatype','double');
+%     h5create(sprintf('/media/roger/48BCFC2BBCFC1562/train%d.h5',i),'/label',size(y(:,:,:,(i-1)*nsamples*nclasses+1:i*nsamples*nclasses)),'Datatype','double');
+% 
+%     h5write(sprintf('/media/roger/48BCFC2BBCFC1562/train%d.h5',i),'/data',X(:,:,:,:,(i-1)*nsamples*nclasses+1:i*nsamples*nclasses));
+%     h5write(sprintf('/media/roger/48BCFC2BBCFC1562/train%d.h5',i),'/label',y(:,:,:,(i-1)*nsamples*nclasses+1:i*nsamples*nclasses));
+% end
+
 
 disp('everythng done here')
